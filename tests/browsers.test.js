1f5e91861e48b9821bd967014d4ea43f48afd675
doinel1a <92680721+doinel1a@users.noreply.github.com>
2023-07-27 14:19:56 +0200

    initial set-up

---
+++ tests/browsers.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable unicorn/prefer-module */
+// @ts-check
+import { test } from '@playwright/test';
+
+import _config from '../_config';
+
+const HOST = _config.server.host;
+const PORT = _config.server.port;
+
+test('Test browsers', async ({ page }) => {
+  await page.goto(`http://${HOST}:${PORT}`);
+  await page.pause();
+});
