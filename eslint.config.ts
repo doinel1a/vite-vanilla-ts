eba71e95a6eaea08df72e07a14546b666149f715
doinel1a <doinel1atanasiu@gmail.com>
2026-01-04 19:44:42 +0100

    chore: migrate to eslint v9

---
+++ eslint.config.ts
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier/flat';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
+import sonarjs from 'eslint-plugin-sonarjs';
+import unicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  js.configs.recommended,
+  sonarjs.configs.recommended,
+  unicorn.configs.recommended,
+  tseslint.configs.recommended,
+  prettierConfig,
+  prettierPlugin,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021
+      }
+    },
+    rules: {
+      // unicorn adjustments
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/no-null': 'off'
+    }
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', '**/*.html']
+  }
+]);
+
